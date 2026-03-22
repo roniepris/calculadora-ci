6cfcca7154f3e01aecea007f1b8eb32c583a5ca3
Ronnie <121251352+roniepris@users.noreply.github.com>
2026-03-22 14:07:27 -0300

    Add basic arithmetic operations in operacoes.py

--- src/calculadora/operacoes.py
+++ src/calculadora/operacoes.py
@@ -1,1 +1,13 @@
+def soma(a, b):
+  return a + b
 
+def subtracao(a, b):
+  return a - b
+
+def multiplicacao(a, b):
+  return a * b
+
+def divisao(a, b):
+  if b == 0:
+    raise ValueError("Divisão por zero")
+  return a / b
